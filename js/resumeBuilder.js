db2804f1b60a54a204991e896c7c07eebd046001
unknown <>
2015-06-23 16:56:52 -0500

    prepare resumeBuilder.js for lesson 2

---
+++ js/resumeBuilder.js
@@ -0,0 +1,43 @@
+
+
+
+var bio = {
+	"name" : "Mario",
+	"role" : "ninja",
+	"contact_info" : {
+		"email" : "123fakeemail@fake.com",
+		"phone" : "713-555-1111"
+	},
+	"picture_url" : "images/my_picture.jpg",
+	"welcome_msg" : "Hi! Welcome to my resume website!",
+	"skills" : ["Programming", "Soccer"]
+	
+};
+
+work = {
+	"employment" : [
+		{
+			"employer" : "Myself",
+			"startDate" : "2010",
+			"position" : "unemployed",
+		}
+	
+	]
+}
+education = {
+	"schools" : [
+		{
+		"name" : "Krusty's Clown College",
+		"startDate" : "2010",
+		"degree" : "Clown Science"
+		}
+	]
+}
+
+projects = {
+	"projects":[
+		{
+			"resume" : "Make a resume using JavaScript"
+		}
+	]
+}
